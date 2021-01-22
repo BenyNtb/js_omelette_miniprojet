4dcf43b40abb5d5052210dd73690da4c973981b4
Beny <b.ntambwe97@gmail.com>
2021-01-22 16:44:19 +0100

    arriver à la maison

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,23 @@
 let personne = {
     nom : 'Beny',
     lieu : 'maison',
-    argent : 100,
+    argent : 10,
     mainDroite : [],
     mainGauche : [],
-    sedeplacer(){
-        // console.log(`${this.nom1 } se déplace à ${this.lieu}`);    
+    sedeplacer(x){
+        this.lieu = x
+        console.log(`${this.nom } se déplace à ${this.lieu.nom}`);    
     },
-    payerArticle(){
-        // console.log(`${this.nom1} paye ses article`);
+    payerArticle(ingredients){
+        this.argent = this.argent - ingredients.prix
+        console.log(`${this.nom} paye ses article`);
     },
     couper(){
-        // console.log(`${this.nom1} coupe ses ${this.ingredients} avec un ${this.outil}`);
+        console.log(`${this.nom} coupe ses ${this.ingredients} avec un ${this.outil}`);
     }
 }
 
-personne.sedeplacer();
+// personne.sedeplacer();
 
 let maison = {
     nom : 'maison',
@@ -42,9 +44,12 @@ let fromage = new Ingredients('fromage', 'coupé', 0.65)
 
 let epicerie = {
     nom : 'epicerie',
-    nom : [],
     personnes : [],
-    paniers : [],
+    paniers : [panier ={
+        type : 'panier',
+        contenu : [],
+    }],
+    ingredients : [oignon, oeuf, epice, fromage]
 }
 // class Lieu {
 //     constructor(nom,personnes,panier) {
@@ -53,12 +58,7 @@ let epicerie = {
 //         this.panier = panier;
 //     }
 // }
-let paniers = [panier ={
-    type : 'panier',
-    contenue : [],
-}]
 
-let ingredients = ['oignon', 'oeuf', 'epice', 'fromage']
 
 let poele = {
     contenue : [0],
@@ -75,17 +75,48 @@ poele.cuir()
 
 let bol = {
     contenu: [],
-    melanger(nomMelange, etat){
-        etat = etat;
+    melanger(nomMelange){
         let newMelange = {
             nomMelange: nomMelange,
             etat: 'pas cuit'
         }
-        this.contenu.push(newMelange)
+        this.contenu = [newMelange]
     }
 }
-
+bol.melanger('omelette')
 console.log(bol);
-bol.melanger('oeuf');
 
-console.log(personne.nom + "est actuellement");
+// OMELETTE
+
+console.log(personne.nom + " est actuellement à la " + personne.lieu);
+personne.sedeplacer(epicerie)
+// console.log(personne.nom + " se déplace à l'" + epicerie.nom);
+
+console.log(personne.nom + " récupère le " + panier.type +  " et le met dans sa main droite");
+epicerie.paniers[panier.contenu.splice(0,1)]
+
+personne.mainDroite.push(epicerie.paniers[0])
+console.log(`${personne.nom} a pris un ${panier.type}`);
+
+// epicerie.ingredient.forEach(el => {
+//     personne.mainDroite[0].contenue.push(el);
+//     console.log(`${personne.nom} a pris un ${el.nom}`);
+// });
+
+epicerie.ingredients.forEach(element => {
+    personne.mainDroite[0].contenu.push(element);
+    console.log(`${personne.nom} a pris un ${element.nom}`);
+})
+
+// console.log(personne.mainDroite);
+
+personne.mainDroite[0].contenu.forEach(element => {
+    personne.payerArticle(element)
+});
+console.log(personne.argent);
+
+personne.sedeplacer(maison)
+
+array.forEach(element => {
+    
+});
